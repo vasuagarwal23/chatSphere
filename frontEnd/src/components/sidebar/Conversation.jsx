7325e683c2189db05ae9144d30a82cc8507ce925
vasu agarwal <vasuagarwal@udits-MacBook-Air-2.local>
2024-04-25 18:11:06 +0530

    index.css file updated

--- frontEnd/src/components/sidebar/Conversation.jsx
+++ frontEnd/src/components/sidebar/Conversation.jsx
@@ -11,8 +11,8 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
 	return (
 		<>
 			<div
-				className={`flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
-				${isSelected ? "bg-sky-500" : ""}
+				className={`flex gap-2 items-center hover:bg-red-100 rounded p-2 py-1 cursor-pointer
+				${isSelected ? "bg-red-100" : ""}
 			`}
 				onClick={() => setSelectedConversation(conversation)}
 			>
@@ -24,7 +24,7 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
 
 				<div className='flex flex-col flex-1'>
 					<div className='flex gap-3 justify-between'>
-						<p className='font-bold text-gray-200'>{conversation.fullName}</p>
+						<p className='font-bold text-black'>{conversation.fullName}</p>
 						<span className='text-xl'>{emoji}</span>
 					</div>
 				</div>
--- frontEnd/src/components/sidebar/SearchInput.jsx
+++ frontEnd/src/components/sidebar/SearchInput.jsx
@@ -32,7 +32,7 @@ const SearchInput = () => {
 				value={search}
 				onChange={(e) => setSearch(e.target.value)}
 			/>
-			<button type='submit' className='btn btn-circle bg-sky-500 text-white'>
+			<button type='submit' className='btn btn-circle bg-red-100 text-black'>
 				<IoSearchSharp className='w-6 h-6 outline-none' />
 			</button>
 		</form>
