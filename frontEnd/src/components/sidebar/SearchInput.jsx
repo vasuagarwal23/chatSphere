4a56e6dbf605bab4635fb8eb4f6de6a479bec954
vasu agarwal <vasuagarwal@udits-MacBook-Air-2.local>
2024-04-26 21:17:27 +0530

    final updates done

--- frontEnd/src/components/sidebar/SearchInput.jsx
+++ frontEnd/src/components/sidebar/SearchInput.jsx
@@ -25,32 +25,17 @@ const SearchInput = () => {
 	};
 	return (
 		<form onSubmit={handleSubmit} className='flex items-center gap-2'>
-			<input
+			<input id="searchInput"
 				type='text'
 				placeholder='Search…'
 				className='input input-bordered rounded-full'
 				value={search}
 				onChange={(e) => setSearch(e.target.value)}
 			/>
-			<button type='submit' className='btn btn-circle bg-red-100 text-black'>
+			<button id="searchButton" type='submit' className='btn btn-circle bg-red-100 text-black'>
 				<IoSearchSharp className='w-6 h-6 outline-none' />
 			</button>
 		</form>
 	);
 };
 export default SearchInput;
-
-// STARTER CODE SNIPPET
-// import { IoSearchSharp } from "react-icons/io5";
-
-// const SearchInput = () => {
-// 	return (
-// 		<form className='flex items-center gap-2'>
-// 			<input type='text' placeholder='Search…' className='input input-bordered rounded-full' />
-// 			<button type='submit' className='btn btn-circle bg-sky-500 text-white'>
-// 				<IoSearchSharp className='w-6 h-6 outline-none' />
-// 			</button>
-// 		</form>
-// 	);
-// };
-// export default SearchInput;
